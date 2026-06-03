2678ad2d7dd22ca789c9d84578cc88b6588565e9
meeranrashith166-lang <meeranrashith166@gmail.com>
2026-06-03 18:18:43 +0530

    Update Chocolatey installer checksum for optimized size

--- bg-remover/tools/chocolateyinstall.ps1
+++ bg-remover/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url           = 'https://github.com/meeranrashith166-lang/BG-Remover/releases/download/v3.0.1/BG_Remover_Setup.exe'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
-  checksum      = '55e676732699a0c7cf4630991c04fef4bb0348d1daf34c3abc661f47a2f11045'
+  checksum      = '3082a39b571d97c8405d21fc0d3146a97fbfa9a5e57259c1ae0e5302633791dc'
   checksumType  = 'sha256'
 }
 
